1dca2ff056891ebf6edc9dc7f61cd98a55d83dc0
PramathShukla164 <pramathshukla275@gmail.com>
2024-12-13 23:48:06 -0700

    Initial Commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,31 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true, 
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+    },
+    fontFamily: {
+      primary: "var(--font-bebas)",
+      secondary: "var(--font-inter)",
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#10151f", 
+        secondary: "#475467",
+        accent: {
+          DEFAULT: "#7edad2",
+          hover: "#79d3cc",
+        }
+      },
+      boxShadow:{
+        custom: "0px 14px 54px rgba(0,0,0,0.1)",
       },
     },
   },
